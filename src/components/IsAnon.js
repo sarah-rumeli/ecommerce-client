0bc4ee15288479653c09972d59109297437ae8de
Rumeli Paul <rumelipaul@gmail.com>
2023-01-25 18:07:00 +0100

    Fixed isAnon

--- src/components/IsAnon.js
+++ src/components/IsAnon.js
@@ -2,20 +2,20 @@ import { useContext } from "react";
 import { AuthContext } from "../context/auth.context";
 import { Navigate } from "react-router-dom";
  
-function IsPrivate( { children } ) {
+function IsAnon( { children } ) {
   
   const { isLoggedIn, isLoading } = useContext(AuthContext);
  
   // If the authentication is still loading 
   if (isLoading) return <p>Loading ...</p>;
  
-  if (!isLoggedIn) {
-  // If the user is not logged in 
-    return <Navigate to="/login" />;
+  if (isLoggedIn) {
+    // If the user is logged in, navigate to the home page     
+    return <Navigate to="/" />;
   } else {
-  // If the user is logged in, allow to see the page 
+    // If the user is not logged in, allow to see the page 
     return children;
   }
 }
  
-export default IsPrivate;
+export default IsAnon;
